287a6dc6f6226e7b160c081b4ddb9fb3eee789af
Emanuel Birge <emanuel.birge@gmail.com>
2013-11-23 16:33:31 +0100

    added level class, test level

--- game/level_generator.lua
+++ game/level_generator.lua
@@ -1,5 +1,3 @@
-local level = require "level"
-
 local LevelGenerator = {}
 LevelGenerator.__index = LevelGenerator
 
@@ -11,11 +9,41 @@ setmetatable(LevelGenerator, {
 
 function LevelGenerator.new()
     local self = setmetatable({}, LevelGenerator)
+    self.lines = 30
+    self.lineWidth = 30
+    self.spawns = 2
+    self.matchExpr = '[XOS]+'
+    self.spawnChar = 'S'
     return self
 end
 
-function LevelGenerator:parse_level(filename)
-    fp = 
+function LevelGenerator:parseLevel(filename)
 end
 
+function LevelGenerator:validateLevel(filename)
+    lineCount = 0
+    spawnPoints = 0
+    for line in io.open(filename):lines() do 
+        lineCount = lineCount + 1
+        if lineCount > self.lines then
+            break
+        end
+        if #line ~= self.lineWidth then 
+            error("line " .. lineCount .. " is not 30 characters wide")
+        end
+        if #string.match(line, self.matchExpr) ~= #line then
+            error("line " .. lineCount .. " contains illegal characters")
+        end
+        if string.find(line, self.spawnChar) then
+            spawnPoints = spawnPoints + #string.match(line, '[S]+')
+        end
+    end
+    if lineCount ~= self.lines then 
+        error("got " .. lineCount .. " lines, expected " .. self.lines)
+    end
+    if spawnPoints ~= self.spawns then
+        error("got " .. spawnPoints .. " spawns, expected " .. self.spawns)
+    end
+end
 
+return LevelGenerator
