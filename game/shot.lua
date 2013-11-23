765c46bfcdb11843de51969bd7a16121e79b37d1
samurainilsson <linus.c.nilsson@gmail.com>
2013-11-23 21:00:59 +0100

    Shots now collides with walls.

--- game/shot.lua
+++ game/shot.lua
@@ -13,6 +13,8 @@ function Shot.init(pos, vel)
 	local shot = {
 		position = {x = pos.x, y = pos.y},
 		velocity = {x = vel.x, y = vel.y},
+		radius = 4,
+		collided = false,
 	}
 	
 	setmetatable(shot, Shot)
@@ -23,11 +25,13 @@ function Shot:update(dt)
 	-- Handle input
 	self.position.x = self.position.x + self.velocity.x * dt
 	self.position.y = self.position.y + self.velocity.y * dt
+	
+	self.collided = level:checkCollisions(self.position, self.radius, false)
 end
 
 function Shot:draw()
 	love.graphics.setColor(math.random(0, 255), math.random(0, 255), math.random(0, 255))
-	love.graphics.circle("fill", self.position.x, self.position.y, 4, 16)
+	love.graphics.circle("fill", self.position.x, self.position.y, self.radius, 16)
 end
 
 return Shot
